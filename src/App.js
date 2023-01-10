849ae67874b173fa01863f5c632e89342ab73729
DLuz1 <diogolinoluz@hollandandbarrett.com>
2023-01-10 12:04:10 +0000

    feat(App): created state for Pokemon and useEffect for fetching from Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { Navigation } from './components/Navigation';
-import { PokemonCard } from './components/PokemonCard';
+import React, { useEffect, useState } from "react";
+import { Navigation } from "./components/Navigation";
+import { PokemonCard } from "./components/PokemonCard";
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch(pokeApi)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemonList(data.results);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }, []);
 
   return (
     <div data-testid="app">
